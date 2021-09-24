0d30ed25bc0767e56f28cbe9223972c7557e59dc
Seth Teichman <smt5541@rit.edu>
2021-09-23 20:22:12 -0400

    Added early termination for Actions, DriveAction for turning and stopping early

--- core/include/utils/action.h
+++ core/include/utils/action.h
@@ -6,25 +6,42 @@
 #include "../core/include/subsystems/tank_drive.h"
 using namespace vex;
 
+/**
+ * action_ptr is a std:function that returns int and takes no params
+ * It was implemented this way because:
+ * 1. We need to pass in lambdas, since lambdas can have captures,
+ * which removes the need for function parameters (which would be a
+ * headache if different functions required different parameters). 
+ * 2. Plain function pointers have issues when you try to pass in a 
+ * lambda that has captures.
+ */
 typedef std::function<int()> action_ptr;
+
 class Action {
   protected:
     action_ptr myFnPtr;
+    bool running;
     Action();
 
   public:
     Action(action_ptr fnPtr);
 
     int run();
+    void stop();
 };
 
 class DriveAction:Action {
   private:
+    DriveAction(TankDrive td);
     DriveAction(TankDrive td, directionType dir, double inches, double pct_speed);
+    DriveAction(TankDrive td, double degrees, double pct_speed);
 
   public:
+    static DriveAction stop_drive(TankDrive td);
     static DriveAction drive_forward(TankDrive td, double dist, double pct_speed);
     static DriveAction drive_reverse(TankDrive td, double dist, double pct_speed);
+    static DriveAction turn_clockwise(TankDrive td, double degrees, double pct_speed);
+    static DriveAction turn_counterclockwise(TankDrive td, double degrees, double pct_speed);
 };
 
 #endif
--- core/src/utils/action.cpp
+++ core/src/utils/action.cpp
@@ -1,40 +1,135 @@
 #include "vex.h"
+#include "vex_task.h"
 #include <functional>
 #include "../core/include/utils/action.h"
 #include "../core/include/subsystems/tank_drive.h"
 using namespace vex;
 
+/**
+ * The Action superclass. Meant to be extended to make purpose-built
+ * reusable actions, or construct an Action directly for a one-off
+ * action.
+ */
 class Action::Action {
   protected:
     action_ptr myFnPtr;
-    Action() {}
+    bool running;
+    Action() {} //Default Constructor so we can extend with no issue
 
   public:
+    /**
+     * Create an Action with an action_ptr.
+     *
+     * @param fnPtr action_ptr to run when Action::run() is called.
+     */
     Action(action_ptr fnPtr) {
       this->myFnPtr = fnPtr;  
     }
 
+    /**
+     * Run and get the result of the function pointed to by myFnPtr.
+     */
     int run() {
+      this->running = true;
       return this->myFnPtr();
+    }
+
+    /**
+     * Get whether this Action is currently running.
+     */
+    bool isRunning() {
+      return this->running;
+    }
+
+    /**
+     * Stop this Action prematurely.
+     */
+    void stop() {
+      this->running = false;
     }
 };
 
+/**
+ * The DriveAction class. Extends Action to be an Action Factory.
+ */
 class DriveAction::DriveAction:Action {
+    /**
+     * Create a DriveAction to halt and reinitialize TankDrive.
+     *
+     * @param td TankDrive instance
+     */
+    DriveAction(TankDrive td) {
+      myFnPtr = [&]() {
+        td.stop();
+        this->running = false;
+        return true;
+      };
+    }
+
+    /**
+     * Create a DriveAction for linear fwd/rev movement in inches.
+     *
+     * @param td TankDrive instance
+     * @param dir directionType direction of movement
+     * @param inches double distance to move
+     * @param pct_speed double percent (0.0-1.0) of power to apply
+     */
     DriveAction(TankDrive td, directionType dir, double inches, double pct_speed) {
       if (dir == directionType::rev) {
         inches = -inches;
       }
       myFnPtr = [&]() {
-        td.drive_forward(inches, pct_speed);
-        return 0;
+        while (td.drive_forward(inches, pct_speed) != true && this->running) {
+          task::sleep(20);
+        }
+        this->running = false;
+        return true;
+      };
+    }
+
+    /**
+     * Create a DriveAction for rotational movement in degrees.
+     * 
+     * @param td TankDrive instance
+     * @param degrees double degrees to rotate (CW=+, CCW=-)
+     * @param pct_speed double percent (0.0-1.0) of power to apply
+     */
+    DriveAction(TankDrive td, double degrees, double pct_speed) {
+      myFnPtr = [&]() {
+        while(td.turn_degrees(degrees, pct_speed) && this->running) {
+          task::sleep(20);
+        }
+        this->running = false;
+        return true;
       };
     }
 
   public:
+    static DriveAction stop_drive(TankDrive td) {
+      return DriveAction(td);
+    }
+    /**
+     * Drive forward with TankDrive a set distance at a defined speed.
+     */
     static DriveAction drive_forward(TankDrive td, double dist, double pct_speed) {
       return DriveAction(td, directionType::fwd, dist, pct_speed);
     }
+    /**
+     * Drive reverse with TankDrive a set distance at a defined speed.
+     */
     static DriveAction drive_reverse(TankDrive td, double dist, double pct_speed) {
       return DriveAction(td, directionType::rev, dist, pct_speed);
+    }
+    /**
+     * Turn CW with TankDrive a set # of degrees at a defined speed.
+     */
+    static DriveAction turn_cw(TankDrive td, double degrees, double pct_speed) {
+      return DriveAction(td, degrees, pct_speed);
+    }
+    /**
+     * Turn CCW with TankDrive a set # of degrees at a defined speed.
+     */
+    static DriveAction turn_ccw(TankDrive td, double degrees, double pct_speed) {
+      return DriveAction(td, -degrees, pct_speed);
     }
 };
