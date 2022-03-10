5816d3e8bd6d1bb24dfc6227bcbcd9b4b3624749
Ryan McGee <ramtech51@comcast.net>
2022-03-09 21:04:28 -0500

    Reformatted robot-config

--- include/robot-config.h
+++ include/robot-config.h
@@ -5,39 +5,35 @@
 using namespace vex;
 
 extern brain Brain;
-
 extern controller main_controller;
 
+// ======== OUTPUTS ========
+
+// Drivetrain Components
 extern motor ldtf, ldtr, ldbf, ldbr, 
              rdtf, rdtr, rdbf, rdbr;
 
-extern motor conveyor_motor, l_lift_motor, r_lift_motor, fork_motor;
-extern pneumatics claw_solenoid;
-
-extern pneumatics rear_clamp, flaps;
-
 extern motor_group left_motors, right_motors, lift_motors;
 
-// extern limit lift_home;
+// Subsystems Components
+extern motor conveyor_motor, l_lift_motor, r_lift_motor, fork_motor;
 
+extern pneumatics claw_solenoid, rear_clamp, flaps;
+
+// ======== INPUTS ========
+extern inertial imu;
+extern vex::distance goal_sense;
+
+// ======== SUBSYSTEMS ========
+
+// Drivetrain
+extern TankDrive drive;
+extern OdometryTank odom;
+
+// Lift
 enum LiftPosition {DOWN, DRIVING, PLATFORM, UP};
 extern std::map<LiftPosition, double> lift_map;
 extern Lift<LiftPosition> lift_subsys;
-// extern RingCollector ring_subsys;
-// extern optical goal_sensor;
-
-// extern encoder left_enc, right_enc;
-
-extern TankDrive drive;
-
-extern OdometryTank odom;
-
-extern inertial imu;
-
-extern vex::distance goal_sense;
-
-
-// VEXcode devices
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -6,43 +6,39 @@ using namespace vex;
 using signature = vision::signature;
 using code = vision::code;
 
-// A global instance of brain used for printing to the V5 Brain screen
 brain  Brain;
+controller main_controller;
 
+// ======== OUTPUTS ========
+
+// Drivetrain Components
 motor ldtf(PORT19, gearSetting::ratio6_1, true), ldtr(PORT18, gearSetting::ratio6_1, true), ldbf(PORT20, gearSetting::ratio6_1), ldbr(PORT17, gearSetting::ratio6_1), 
       rdtf(PORT13, gearSetting::ratio6_1), rdtr(PORT15, gearSetting::ratio6_1), rdbf(PORT12, gearSetting::ratio6_1, true), rdbr(PORT14, gearSetting::ratio6_1, true);
 
 motor_group left_motors = {ldtf, ldtr, ldbf, ldbr};
 motor_group right_motors = {rdtf, rdtr, rdbf, rdbr};
 
+// Subsystems Components
 motor conveyor_motor(PORT11, true), l_lift_motor(PORT1, true), r_lift_motor(PORT2);
-
 motor_group lift_motors = {l_lift_motor, r_lift_motor};
 
 pneumatics claw_solenoid(Brain.ThreeWirePort.B);
-
 pneumatics rear_clamp(Brain.ThreeWirePort.H);
-
 pneumatics flaps(Brain.ThreeWirePort.C);
 
-// limit lift_home(Brain.ThreeWirePort.B);
-
-// optical goal_sensor(PORT15);
-
-// encoder left_enc(Brain.ThreeWirePort.A), right_enc(Brain.ThreeWirePort.C);
-
+// ======== INPUTS ========
 inertial imu(PORT16);
-
 vex::distance goal_sense(PORT10);
 
+// ======== SUBSYSTEMS ========
+
+// General Robot Configuration
 robot_specs_t robot_cfg = {
   .robot_radius = 12, // inches
   .odom_wheel_diam = 2.85,//4.25, // inches
   .odom_gear_ratio = 1.03,//2.333333, // inches
   .dist_between_wheels = 9.75, // inches
-
   .drive_correction_cutoff = 12, //inches
-
   .drive_pid = (PID::pid_config_t) 
   {
     .p = .5,
@@ -53,7 +49,6 @@ robot_specs_t robot_cfg = {
     .deadband = 0.3,
     .on_target_time = 0
   },
-
   .turn_pid = (PID::pid_config_t)
   {
     .p = 0.12,
@@ -63,29 +58,18 @@ robot_specs_t robot_cfg = {
     .deadband = 2,
     .on_target_time = 0.1
   },
-
   .correction_pid = (PID::pid_config_t)
   {
     .p = 0.02,
     .d = 0.002
   }
-
 };
 
-PID::pid_config_t fork_pid_cfg = {
-  .p = 50000,
-  .i = 0,
-  .d = 0,
-  .f = 0,
-
-  .deadband = .1,
-  .on_target_time = .1
-};
-
+// Drivetrain
 OdometryTank odom(left_motors, right_motors, robot_cfg, &imu);
-
 TankDrive drive(left_motors, right_motors, robot_cfg, &odom);
 
+// Lift
 Lift<LiftPosition>::lift_cfg_t lift_cfg = {
   .up_speed = 12, //volts
   .down_speed = 6, //volts / second
@@ -111,14 +95,6 @@ map<LiftPosition, double> lift_map {
 };
 
 Lift<LiftPosition> lift_subsys(lift_motors, lift_cfg, lift_map);
-// RingCollector ring_subsys(fork_motor, conveyor_motor, goal_sensor, lift_subsys, fork_pid_cfg);
-
-controller main_controller;
-
-// VEXcode device constructors
-
-// VEXcode generated functions
-
 
 
 /**
