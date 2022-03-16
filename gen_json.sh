f6fff6c5093df5145c7414a1f38c5821195342fd
Ryan McGee <ramtech51@comcast.net>
2022-03-15 20:16:48 -0400

    Fixed missing files from v5; added json script

---
+++ gen_json.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+
+# Search for source/header files, excluding those in the .git directory
+files=$(find -name "*.cpp" |grep -v "git" |sed "s|./||")
+files="${files} $(find -name "*.h" |grep -v "git" |sed "s|./||")"
+files="${files} $(find -name "*.md" |grep -v "git" |sed "s|./||")"
+
+# Search for directories under src, include or core
+directories=$(find -type d -regex "./\(core\|src\|include\)/.*" |sed "s|./||")
+
+output=$(mktemp)
+
+# generate the "File" type of json
+for i in ${files}; do
+	builder="{\"name\": \"$i\", \"type\": \"File\", \"specialType\":\"\"},"
+	echo "${builder}" >> $output
+done
+
+# generate the "Folder" type of json
+for i in ${directories}; do
+	builder="{\"name\": \"$i\", \"type\": \"Directory\"},"
+	echo "${builder}" >> $output
+done
+
+sed -i "$ s/,$//" $output
+
+cat $output
